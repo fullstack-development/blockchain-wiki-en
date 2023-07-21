9c54cc2f626890f57067563f498f8b8a1e4d2177
Nikolay Bordunenko <100994840+indigonico@users.noreply.github.com>
2023-07-21 11:51:25 +0700

    Update SignatureVerifier.sol

--- concepts/digital-signature-on-ethereum/examples/src/SignatureVerifier.sol
+++ concepts/digital-signature-on-ethereum/examples/src/SignatureVerifier.sol
@@ -2,19 +2,19 @@
 pragma solidity ^0.8.18;
 
 /**
- * @notice Контракт проверяет подписанное приватным ключом произвольное сообщение
- * @dev Используется встроенная функция ecrecover()
+ * @notice The contract verifies an arbitrarily signed message using a private key.
+ * @dev It uses the built-in function ecrecover().
  */
 contract SignatureVerifier {
-    /// @notice Префикс для обозначения, что эта подпись будет использоваться только внутри сети Ethereum
+    /// @notice The prefix to indicate that this signature will be used only within the Ethereum network.
     bytes32 constant public PREFIX = "\x19Ethereum Signed Message:\n32";
 
-    /// @notice Проверяет была ли подпись сделана адресом signer
+    /// @notice Verifies whether the signature was made by the address "signer."
     function isValid(address signer, bytes32 hash, uint8 v, bytes32 r, bytes32 s) external pure returns (bool) {
         return _recover(hash, v, r, s) == signer;
     }
 
-    /// @notice Восстанавливает публичный адрес приватного ключа, которым была сделана передаваямая подпись
+    /// @notice Recovers the public address of the private key that was used to make the given signature.
     function _recover(bytes32 hash, uint8 v, bytes32 r, bytes32 s) private pure returns (address) {
         bytes32 prefixedHash = keccak256(abi.encodePacked(PREFIX, hash));
 
