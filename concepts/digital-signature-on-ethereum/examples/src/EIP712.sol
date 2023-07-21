077e20d444edc3c42ebffa4c49b0b62f6e94e695
Nikolay Bordunenko <100994840+indigonico@users.noreply.github.com>
2023-07-21 11:49:06 +0700

    Update EIP712.sol

--- concepts/digital-signature-on-ethereum/examples/src/EIP712.sol
+++ concepts/digital-signature-on-ethereum/examples/src/EIP712.sol
@@ -4,19 +4,19 @@ pragma solidity ^0.8.18;
 import {ECDSA} from "openzeppelin-contracts/utils/cryptography/ECDSA.sol";
 
 /**
- * @notice Контракт проверяет подписанное приватным ключом сообщение c типизированными данными согласно EIP-712.
- * @dev Используется библиотека от OpenZeppelin ECDSA
+ *@notice The contract verifies a message signed with a private key using typed data according to EIP-712.
+ * @dev It uses the ECDSA library from OpenZeppelin.
  */
 contract EIP712 {
     bytes32 public constant IS_VALID_TYPEHASH = keccak256("isValid(uint256 nonce)");
 
-    /// @notice Счетчик проверки подписи. Позволяет быть уверенным, что одна и таже подпись не бует использована дважды
+/// @notice Signature verification counter. Ensures that the same signature is not used twice.
     uint256 public signatureNonce;
 
     error SignatureIsInvalid();
 
-    /// @notice 32-байтовый разделитель домена. Используется для определения свойств конкретного приложения.
-    /// Другими словами подпись может использоваться только для этого приложения
+    /// @notice 32-byte domain delimiter. Used to determine the properties of a specific application.
+/// In other words, the signature can only be used for this application.
     function DOMAIN_SEPARATOR() public view returns (bytes32) {
         return keccak256(
             abi.encode(
@@ -29,11 +29,12 @@ contract EIP712 {
         );
     }
 
-    /// @notice hashStruct. Используется для определения типизированных данных подписи
+    /// @notice hashStruct. Used to determine the typed data of the signature.
+
     function _getDigest(bytes32 typeHash) private view returns (bytes32) {
         return keccak256(
             abi.encodePacked(
-                "\x19\x01", // Согласно EIP-191. Фиксированное значение версии. Определяет "Structured data" EIP-712
+                "\x19\x01", // According to EIP-191, it represents a fixed version value that defines "Structured data" EIP-712.
                 DOMAIN_SEPARATOR(),
                 keccak256(
                     abi.encode(
@@ -46,9 +47,9 @@ contract EIP712 {
     }
 
     /**
-     * @notice Проверяет была ли подпись сделана адресом signer
-     * @param signer Публичный адрес для проверки, подписавший сообщение
-     * @param signature Проверяемая подпись (abi.encoded(r, s, v))
+     * @notice Verifies if the signature was made by the signer's address.
+     * @param signer The public address to check, the one that signed the message.
+     * @param signature The signature to be verified (abi.encoded(r, s, v)).
      */
     function isValid(address signer, bytes memory signature) public view returns (bool) {
         bytes32 digest = _getDigest(IS_VALID_TYPEHASH);
