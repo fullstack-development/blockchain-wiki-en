0170aa2e7963676d4f0649a86d0972007c2d55fd
Nikolay Bordunenko <100994840+indigonico@users.noreply.github.com>
2023-07-22 22:36:30 +0700

    Update SimpleProxy.sol

--- concepts/upgradeable-contracts/method-3/contracts/SimpleProxy.sol
+++ concepts/upgradeable-contracts/method-3/contracts/SimpleProxy.sol
@@ -1,30 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-/// @notice Контракт с логикой реализации
+/// @notice A cantract with implementation logic
 contract Logic {
     address public initAddress;
     uint256 private _value;
 
     /**
-     * @notice Используется для установки данных на контракт при инициализации
-     * @dev По сути заменитель constructor()
+     * @notice Used to set data on the contract during initialization.
+ * @dev In reality, it's simply a replacement for constructor().
      */
     function initialize(address _initAddress) public {
         initAddress = _initAddress;
     }
 
     /**
-     * @notice Позволяет записать значение в state
-     * @param _newValue Новое значение для записи в state
+     * @notice Allows writing a value to the state.
+ * @param _newValue The new value to be written to the state.
      */
     function store(uint256 _newValue) public {
         _value = _newValue;
     }
 
     /**
-     * @notice Позволяет получить значение из state
-     * @return _value Значение из state
+     * @notice Allows retrieving a value from the state.
+     * @return _value A value from state 
      */
     function retrieve() public view returns (uint256) {
         return _value;
@@ -32,10 +32,10 @@ contract Logic {
 }
 
 /**
- * @notice Контракт прокси
- * @dev Не имеет реализации. Будет делегировать вызов контракту логики.
- * Фактическое хранение данных будет на контракте прокси.
- * Взаимодействие с контрактом логики только через вызовы на прокси контракте
+ * @notice Proxy Contract
+ * @dev It does not have its own implementation. It will delegate calls to the logic contract.
+ * The actual data storage will be on the proxy contract.
+ * Interaction with the logic contract can only happen through calls to the proxy contract.
  */
 contract Proxy {
     struct AddressSlot {
@@ -43,10 +43,9 @@ contract Proxy {
     }
 
     /**
-     * @notice Внутренняя переменная для определения места записи информации об адресе контракта логики
-     * @dev Согласно EIP-1967 слот можно рассчитать как bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1));
-     * Выбираем псевдослучайный слот и записывае адрес контракта логики в этот слот. Эта позиция слота должна быть достаточно случайной,
-     * чтобы переменная в контракте логики никогда не занимала этот слот.
+     * @notice Internal variable used to determine the storage location for the address of the logic contract.
+ * @dev According to EIP-1967, the slot can be calculated as bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1).
+ * We choose a pseudo-random slot and store the address of the logic contract in this slot. The slot position should be random enough to ensure that no variable in the logic contract ever occupies this slot.
      */
     bytes32 internal constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
@@ -54,30 +53,30 @@ contract Proxy {
         _setImplementation(logic);
     }
 
-    /// @notice Возращает адрес установленного контракта логики для контракта прокси
+    /// @notice Returns the address of the set logic contract for the proxy contract.
     function getImplementation() external view returns (address) {
         return _getImplementation();
     }
 
-    /// @notice Устанавливает адрес контракта логики для контракта прокси
+    /// @notice Sets the address of the logic contract for the proxy contract.
     function setImplementation(address _newLogic) external {
         _setImplementation(_newLogic);
     }
 
     function _delegate(address _implementation) internal {
-        // Необходима assembly вставка, потому что невозможно получить доступ к слоту для возврата значения в обычном solidity
+        // An assembly insertion is required because it is not possible to directly access the slot to return the value in regular Solidity.
         assembly {
-            // Копируем msg.data и получаем полный контроль над памятью для этого вызова.
+            // Copying msg.data and gain full control over the memory for this invocation.
             calldatacopy(0, 0, calldatasize())
 
-            // Вызываем контракт реализации
+            // calling the implementation contract.
             let result := delegatecall(gas(), _implementation, 0, calldatasize(), 0, 0)
 
-            // Копируем возвращаемые данные
+            // Copying the returned data.
             returndatacopy(0, 0, returndatasize())
 
             switch result
-            // Делаем revert, если возвращенные данные равны нулю.
+            // Do revert, If the returned data is equal to zero.
             case 0 {
                 revert(0, returndatasize())
             }
@@ -88,24 +87,24 @@ contract Proxy {
     }
 
     /**
-     * @notice Возращает адрес установленного контракта логики для контракта прокси
-     * @dev Адрес логики хранится в специально отведенном слоте, для того, чтобы невозможно было случайно затереть значение
+     * @notice Returns the address of the set logic contract for the proxy contract.
+ * @dev The logic address is stored in a specifically allocated slot to prevent accidentally overwriting the value.
      */
     function _getImplementation() internal view returns (address) {
         return getAddressSlot(_IMPLEMENTATION_SLOT).value;
     }
 
     /**
-     * @notice Устанавливает адрес контракта логики для котракта прокси
-     * @dev Адрес логики хранится в специально отведенном слоте, для того, чтобы невозможно было случайно затереть значение
+     * @notice Sets the address of the logic contract for the proxy contract.
+ * @dev The logic address is stored in a specifically allocated slot to prevent accidentally overwriting the value.
      */
     function _setImplementation(address newImplementation) private {
         getAddressSlot(_IMPLEMENTATION_SLOT).value = newImplementation;
     }
 
     /**
-     * @notice Возвращает произвольный слот памяти типа storage
-     * @param slot Указатель на слот памяти storage
+     * @notice Returns the content of an arbitrary storage memory slot.
+ * @param slot Pointer to the storage memory slot.
      */
     function getAddressSlot(bytes32 slot) internal pure returns (AddressSlot storage r) {
         assembly {
@@ -113,8 +112,8 @@ contract Proxy {
         }
     }
 
-    /// @dev Любые вызовы функций контракта логики через прокси будут делегироваться благодаря обработке внутри fallback
-    fallback() external {
+/// @dev Any calls to functions of the logic contract through the proxy will be delegated thanks to the handling inside the fallback function.
+fallback() external {
         _delegate(_getImplementation());
     }
 }
