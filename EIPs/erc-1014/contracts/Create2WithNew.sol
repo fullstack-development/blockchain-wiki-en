65c904bddca4cc51d9b44648cd699e2bfcde2322
prikolotama <90691319+prikolotama@users.noreply.github.com>
2025-07-18 12:42:12 +0700

    Update Create2WithNew.sol

--- EIPs/erc-1014/contracts/Create2WithNew.sol
+++ EIPs/erc-1014/contracts/Create2WithNew.sol
@@ -12,14 +12,14 @@ contract Bar {
 }
 
 contract DeployerCreate2 {
-    /// @notice Создание контракта через create2 без отправки ETH на новый адрес
+    /// @notice Creating a contract via create2 without sending ETH to the new address
     function create2Foo(bytes32 _salt) external returns (address) {
         Foo foo = new Foo{salt: _salt}();
 
         return address(foo);
     }
 
-    /// @notice Создание контракта через create2 с отправкой ETH на новый адрес
+    /// @notice Creating a contract via create2 with sending ETH to the new address
     function create2Bar(bytes32 _salt) external payable returns (address) {
         Bar bar = new Bar{value: msg.value, salt: _salt}();
 
